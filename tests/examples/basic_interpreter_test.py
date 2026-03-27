04337132b8183ef5d15cfeccfbb0a802fa93bd2e
Zvika Ben-Haim <zvikabh@gmail.com>
2026-03-27 13:33:01 +0300

    add test for prime number calculator

--- tests/examples/basic_interpreter_test.py
+++ tests/examples/basic_interpreter_test.py
@@ -65,6 +65,37 @@ class BasicInterpreterTest(unittest.TestCase):
 *****
 ''')
 
+    def test_calc_primes(self) -> None:
+        program = '''
+            n = 2
+            10 div = 2
+            20 if div >= n then goto 40 end if
+            mult = div
+            30 if mult = n then goto 50 end if
+            if mult > n then
+                div = div + 1
+                goto 20
+            else
+                mult = mult + div
+                goto 30
+            end if
+            40 print n
+            50 n = n + 1
+            if n < 20 then goto 10 end if
+        '''
+        runner = basic_interpreter.BasicInterpreter(program)
+        output = ''.join(runner.exec())
+        self.assertEqual(output, '''\
+2
+3
+5
+7
+11
+13
+17
+19
+''')
+
 
 if __name__ == '__main__':
     unittest.main()
